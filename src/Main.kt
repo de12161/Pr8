2864e88e97fd58a78bd673f21289344864e081bd
de12161 <162108835+de12161@users.noreply.github.com>
2024-11-30 00:46:07 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
+private fun inputInt(message: String): Int {
+    while (true) {
+        print(message)
+        val input = readln().toIntOrNull()
+
+        if (input == null) {
+            println("Введите число")
+            continue
+        }
+
+        if (input <= 0) {
+            println("Число должно быть больше нуля")
+            continue
+        }
+
+        return input
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    val forkPhilosopherCount = inputInt("Введите число философов за столом с вилками: ")
+    val forkTable = Table(forkPhilosopherCount) { name, seat -> ForkPhilosopher(name, seat) }
+    forkTable.takeUtensils()
+
+    println()
+
+    val stickPhilosopherCount = inputInt("Введите число философов за столом с палочками: ")
+    val stickTable = Table(stickPhilosopherCount) { name, seat -> StickPhilosopher(name, seat) }
+    stickTable.takeUtensils()
 }
